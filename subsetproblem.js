41193acbd2d461d74888c82d0a6d5d648719ccc7
sherman94062 <sherman94062@yahoo.com>
2014-12-15 14:39:23 -0600

    Update subsetproblem.js

--- subsetproblem.js
+++ subsetproblem.js
@@ -16,26 +16,22 @@
 // 3, 4, 9, 14, 15, 19, 28, 37, 47, 50, 54, 56, 59, 61, 70, 73, 78, 81,
 // 92, 95, 97, 99
 //
-//var theSet = [3, 4, 9, 14, 15, 19, 28, 37, 47, 50, 54, 56, 59, 61, 70, 73, 78, 81, 92, 95, 97, 99];
-var theSet = [1,2,3,4,6];
+var theSet = [3, 4, 9, 14, 15, 19, 28, 37, 47, 50, 54, 56, 59, 61, 70, 73, 78, 81, 92, 95, 97, 99];
+//var theSet = [1,2,3,4,6];
 var subsetCount = 0;
 var sumArray = [];
 // iterate through the set left to right, assuming sorted positive integers
 for (ii = 0, len = theSet.length; ii < len; ii++) {
-  for (jj = 0, slen = sumArray.length; jj < slen; jj++) {
+  for (jj = 0, slen = sumArray.length; jj < slen; jj++) { // see if we have a match in the sumArray accumulated sums
     if (sumArray[jj] == theSet[ii]) {
-      console.log('Found new subset: ', sumArray[jj]);
       subsetCount++;
     }
   }
   for (jj = 0; jj < slen; jj++) {
     if (sumArray[jj]+theSet[ii] <= theSet[len-1]) { // add this combination to the sums that we check for
       sumArray.push(sumArray[jj]+theSet[ii]);
-      console.log('Added new sumArray1: ', sumArray[jj]+theSet[ii]);
     }
   }
   sumArray.push(theSet[ii]); // add each item in the array for comparison purposes
-  console.log('Added new sumArray2: ', theSet[ii]);
-
 }
 console.log('The answer is: ', subsetCount);
